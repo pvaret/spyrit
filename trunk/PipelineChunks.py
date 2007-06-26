99494e1e001c4728fed294d61136b94adc8e9048
balinares <devnull@localhost>
2007-06-26 11:53:50 +0200

    [svn r46] Added explicit GPL licensing to all files.

--- trunk/PipelineChunks.py
+++ trunk/PipelineChunks.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+## Copyright (c) 2002-2007 Pascal Varet <p.varet@gmail.com>
+##
+## This file is part of Spyrit.
+##
+## Spyrit is free software; you can redistribute it and/or modify it under the
+## terms of the GNU General Public License version 2 as published by the Free
+## Software Foundation.
+##
+## You should have received a copy of the GNU General Public License along with
+## Spyrit; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+## Fifth Floor, Boston, MA  02110-1301  USA
+##
+
 ##
 ## PipelineChunks.py
 ##
--- trunk/PipelineFilters.py
+++ trunk/PipelineFilters.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+## Copyright (c) 2002-2007 Pascal Varet <p.varet@gmail.com>
+##
+## This file is part of Spyrit.
+##
+## Spyrit is free software; you can redistribute it and/or modify it under the
+## terms of the GNU General Public License version 2 as published by the Free
+## Software Foundation.
+##
+## You should have received a copy of the GNU General Public License along with
+## Spyrit; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+## Fifth Floor, Boston, MA  02110-1301  USA
+##
+
 ##
 ## PipelineFilters.py
 ##
@@ -316,7 +331,7 @@ class UnicodeTextFilter( BaseFilter ):
   
   relevant_types = [ chunktypes.BYTES ]
   
-  def __init__(s, *args):
+  def __init__( s, *args ):
     
     BaseFilter.__init__( s, *args )
     s.encoding = "ascii"

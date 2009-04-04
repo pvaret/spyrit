3f91ad44f164519bd5097e9599fad122e5d2da8a
kefen <devnull@localhost>
2009-04-04 04:04:43 +0200

    [svn r215] Implemented automatic process to create release images for Mac OS X (tested on 10.5.6) using py2app, hdutils and a lightweight template DMG image already containing the nice formatting.

---
+++ trunk/dist/darwin/build.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import math
+import os
+import sys
+
+from setuptools import setup
+
+
+this_dir   = os.path.dirname( __file__ )
+
+APPNAME    = "Spyrit" # TODO: get from spyrit itself
+VERSION    = "0.4"    # TODO: get from spyrit itself
+
+RM         = "rm"
+BUILDDIR   = os.path.join( this_dir, "build" )
+DISTDIR    = os.path.join( this_dir, "dist" )
+IMGGLOB    = "%s-*.*.dmg %s-*.*.sparseimage" % ( APPNAME, APPNAME )
+
+APP        = [ os.path.join( "..", "..", "%s.py" % APPNAME ) ]
+ICON_FILE  = os.path.join( "..", "..", "resources", "spyrit-icon.icns" )
+INCLUDES   = [ "PyQt4._qt", "sip" ]
+OPTIONS    = { "argv_emulation": True, 
+               "iconfile": ICON_FILE, 
+               "includes": INCLUDES }
+  
+HDIUTIL    = "hdiutil"
+MKDIR      = "mkdir"
+CP         = "cp"
+MOUNTROOT  = os.path.join( this_dir, "mnt" )
+CONVERT2SP = "convert %s-template.dmg -format UDSP -o %s-%s" \
+           % ( APPNAME, APPNAME, VERSION )
+CONVERT2DMG= "convert %s-%s.sparseimage -format UDBZ -o %s-%s.dmg" \
+           % ( APPNAME, VERSION, APPNAME, VERSION )
+RESIZE     = "resize -size %%sm %s-%s.sparseimage" % ( APPNAME, VERSION )
+MOUNT      = "mount %s-%s.sparseimage -mountroot %s" \
+           % ( APPNAME, VERSION, MOUNTROOT )
+EJECT      = "eject %s" % os.path.join( MOUNTROOT, APPNAME )
+DUSAGE     = "du"
+OVERHEAD   = 9.1  # Size already taken in empty DMG 
+APPDIR     = os.path.join( this_dir, "dist", "%s.app" % APPNAME, "Contents" )
+PACKDIR    = os.path.join( MOUNTROOT, "%s" % APPNAME, "%s.app" % APPNAME )
+
+
+def cleanup():
+
+  ## Removes build and dist folders as well as old images before running setup.
+  
+  os.system( " ".join( [ RM, "-rf", BUILDDIR ] ) )
+  os.system( " ".join( [ RM, "-rf", DISTDIR ]  ) )
+  os.system( " ".join( [ RM, "-f", IMGGLOB ]  ) )
+  
+
+def build():
+
+  ## Uses py2app to create Mac application from source.
+
+  sys.argv.append( "py2app" )
+
+  setup( app            = APP,
+         data_files     = [],
+         options        = { "py2app": OPTIONS },
+         setup_requires = [ "py2app" ] )
+
+
+def package():
+
+  ## Packages the built .app into a shiny DMG template we'll resize
+  ## on the spot to fit the genrated application's size as closely as
+  ## possible (to within a couple MB).
+
+  ## We first need to compute the necessary size for our final DMG.
+  ## Then we'll resize our template DMG, mount it and copy the app. Then
+  ## we unmount it, convert it to a compressed DMG and we're done.
+  
+  appSize  = os.popen( " ".join( [ DUSAGE, "-sm", APPDIR ] ) ).read().split()[0]
+  appSize  = float( appSize )
+  fullSize = int( math.ceil( appSize + OVERHEAD ) )
+  
+  os.system( " ".join( [ HDIUTIL, CONVERT2SP ] ) )
+  os.system( " ".join( [ HDIUTIL, RESIZE % fullSize ] ) )
+  
+  ## Custom mountpoint must exist
+  
+  os.system( " ".join( [ MKDIR, MOUNTROOT ] ) )
+  
+  os.system( " ".join( [ HDIUTIL, MOUNT ] ) )
+  os.system( " ".join( [ CP, "-R", APPDIR, PACKDIR ] ) )
+  os.system( " ".join( [ HDIUTIL, EJECT ] ) )
+  os.system( " ".join( [ HDIUTIL, CONVERT2DMG ] ) )  
+
+
+if __name__ == "__main__":
+
+  cleanup()
+  build()
+  package()

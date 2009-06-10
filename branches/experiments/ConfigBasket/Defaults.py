69632dd85d86f1d071e789672692c7dd23382bd8
sundance <devnull@localhost>
2009-06-10 22:16:39 +0200

    Cleanup from SVN: create Hg branch for v0.1b.

--- branches/experiments/ConfigBasket/Defaults.py
+++
@@ -1,7 +0,0 @@
-SPECS = (
-##  ("option_name", option_default_value, """option doc""", option_validator),
-  ("size", (320, 200)),
-  ("input_text_color", "#000000"),
-  ("input_background_color", "#FFFFFF")
-)
-

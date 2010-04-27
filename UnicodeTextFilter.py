21b6947a31622012c7a5c88a2f679161b66fc744
sundance <devnull@localhost>
2010-04-28 00:22:35 +0200

    Removed workaround meant for Python < 2.5.

--- UnicodeTextFilter.py
+++ UnicodeTextFilter.py
@@ -17,9 +17,9 @@
 ## UnicodeTextFilter.py
 ##
 ## This file holds the UnicodeTextFilter class, which converts the incoming
-## byte stream on the fly into Unicode text. It also contains the helper class
-## StreamingDecoder for versions of Python < 2.5.
+## byte stream on the fly into Unicode text.
 ##
+
 
 import re
 import codecs
@@ -30,60 +30,16 @@ from PipelineChunks import chunktypes, UnicodeTextChunk
 
 
 
-class StreamingDecoder:
-
-  ## WORKAROUND: Python 2.4 doesn't come with an incremental Unicode decoder,
-  ## so we need to provide one until Python 2.5 is installed everywhere.
-  ## For this purpose, we use the classic StreamReader class of the codecs
-  ## module, and we feed data through a dummy file-like buffer object.
-  ## This code will go when we stop supporting Python 2.4.
-
-  class Buffer:
-
-    ## Small file-like buffer class meant for use by the Unicode stream decoder.
-
-    def __init__( s ):
-
-      s._buffer = ""
-
-
-    def write( s, str ):
-
-      s._buffer = str
-
-
-    def read( s ):
-
-      data      = s._buffer
-      s._buffer = ""
-      return data
-
-
-  def __init__( s, encoding, errors="strict" ):
-
-    s.buffer  = s.Buffer()
-    s.decoder = codecs.getreader( encoding ) ( s.buffer, errors )
-  
-
-  def decode( s, str ):
-
-    s.buffer.write( str )
-    return s.decoder.read()
-
-
-  def reset( s ):
-
-    s.buffer.read()
-    s.decoder.reset()
-
-
 
 class UnicodeTextFilter( BaseFilter ):
-  
+
   relevant_types = chunktypes.BYTES
 
 
   def __init__( s, context, encoding ):
+
+    s.decoder  = None
+    s.encoding = "ascii"
 
     BaseFilter.__init__( s, context )
 
@@ -105,31 +61,23 @@ class UnicodeTextFilter( BaseFilter ):
       encoding = "latin1"
 
     s.encoding = encoding
-
-    s.makeStreamDecoder()
-
-
-  def makeStreamDecoder( s ):
-
-    if hasattr( codecs, "getincrementaldecoder" ):  ## True as of Python 2.5
-      s.decoder = codecs.getincrementaldecoder( s.encoding ) ( "replace" )
-
-    else:
-      s.decoder = StreamingDecoder( s.encoding, "replace" )
+    s.decoder  = codecs.getincrementaldecoder( s.encoding ) ( "replace" )
 
 
   def resetInternalState( s ):
 
     BaseFilter.resetInternalState( s )
 
-    if hasattr( s, "decoder" ): s.decoder.reset()
+    if s.decoder:
+      s.decoder.reset()
 
 
   def processChunk( s, chunk ):
- 
+
     text = s.decoder.decode( chunk.data )
 
-    if text: yield UnicodeTextChunk( text )
+    if text:
+      yield UnicodeTextChunk( text )
 
 
   def formatForSending( s, data ):

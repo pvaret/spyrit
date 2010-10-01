ba796028c568082188be6e7756442c07b357af06
sundance <devnull@localhost>
2010-10-01 12:55:19 +0200

    Overhauled About dialog; mentioned contributors.

--- AboutDialog.py
+++ AboutDialog.py
@@ -24,34 +24,54 @@ from localqt           import *
 from PrettyPanelHeader import PrettyPanelHeader
 
 
-ABOUT = u"""
-  <center><br/>
-  <font size="+2"><b>%(app_name)s v%(app_version)s</b></font><br/>
-  <br/>
-  "Still alive!"<br/>
-  <br/>
-  This software is a beta version and as of yet incomplete, although it is
-  already usable and, we hope, fast, stable and pleasant to use.<br/>
-  <br/>
-  Please stay tuned for further developments! We aim to release version 1.0
-  within a few months, after adding the important features that are currently
-  missing, namely matches, highlights and such, and configuration dialogs for
-  everything.<br/>
-  <br/>
-  %(app_name)s is &#169;2007-2010 P. Varet, and licensed under the
+ABOUT = u"""\
+<center><font size="+2">
+  <b>%(app_name)s %(app_version)s</b></font><br/>
+<br/>
+"Endorsed by scientists!"</center>
+<br/>
+<br/>
+%(app_name)s is a client for textual roleplaying games such as MUDs, MUSHes,
+  MUCKs, MOOs and other M-acronyms.<br/>
+<br/>
+Though it is still in development and far from complete, it tries really hard
+  to be polished and pleasant to use. Thank you for trying it out! Feedback and
+  bug reports can be sent to
+  <a href="mailto:spyrit-devel@lists.sourceforge.net">
+  spyrit-devel@lists.sourceforge.net</a>.<br/>
+<br/>
+Technically minded users can follow the development on <a
+  href="https://www.ohloh.net/p/spyrit">Ohloh</a> and get the latest source
+  code from <a href="http://bitbucket.org/balinares/spyrit">BitBucket</a>.<br/>
+<br/>
+This software uses the <i><a
+  href="http://code.activestate.com/recipes/473846/">winpath</a></i> module by
+  Chris Arndt, and a modified version of Raymond Hettinger's <i><a
+  href="http://pypi.python.org/pypi/ordereddict">OrderedDict</a></i>. Special
+  characters imported from the <a href="http://dejavu.sourceforge.net/">DejaVu
+  Sans Mono</a> font. Additional icons imported from the <a
+  href="http://www.oxygen-icons.org/">Oxygen</a> set.<br/>
+<br/>
+%(app_name)s is &#169;2007-2010 P. Varet, and licensed under the
   <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html">GNU
   General Public License</a>.<br/>
-  <br/>
-  This software uses the
-  <i><a href="http://code.activestate.com/recipes/473846/">winpath</a></i>
-  module by Chris Arndt.<br/>
-  Dingbat symbols imported from the
-  <a href="http://dejavu.sourceforge.net/">DejaVu Sans Mono</a> font.<br/>
-  Horse logo contributed by
-  <a href="http://waltervermeij.blogspot.com/">Walter Vermeij</a>.
-  </center>
-  <br/>
-  <br/>
+"""
+
+
+AUTHORS = u"""\
+<b><font size="+2">Authors</font></b><br/>
+<br/>
+<b>Pascal Varet</b>: Project lead, code design, community management, Web site
+  maintenance, pony wrangling.<br/>
+<b>Clément Mairet</b>: Additional programming, spyrcc maintenance.<br/>
+<br/>
+<br/>
+<b><font size="+2">Contributors</font></b><br/>
+<br/>
+<b>Matthew Groat</b>: Idea and initial patch for ANSI colors in logs and sound
+  play on pattern match.<br/>
+<b>Walter Vermeij</b>: Horse logo design.<br/>
+<b>Griatch</b>: Review, server-side feedback.<br/>
 """
 
 
@@ -64,7 +84,8 @@ class AboutDialog( QtGui.QDialog ):
     s.setLayout( QtGui.QVBoxLayout( s ) )
 
     min_size = config._mainwindow_min_size
-    if len( min_size ) >= 2: s.setMinimumSize( min_size[0], min_size[1] )
+    if len( min_size ) >= 2:
+      s.setMinimumSize( min_size[0], min_size[1] )
 
     title = u"About %s" % config._app_name
 
@@ -73,11 +94,20 @@ class AboutDialog( QtGui.QDialog ):
     header = PrettyPanelHeader( title, QtGui.QPixmap( ":/app/icon" ) )
     s.layout().addWidget( header )
 
-    label = QtGui.QLabel( ABOUT % config )
-    label.setWordWrap( True )
-    label.setOpenExternalLinks( True )
+    about = QtGui.QLabel( ABOUT % config )
+    about.setContentsMargins( 20, 20, 20, 20 )
+    about.setWordWrap( True )
+    about.setOpenExternalLinks( True )
 
-    s.layout().addWidget( label )
+    authors = QtGui.QLabel( AUTHORS % config )
+    authors.setContentsMargins( 20, 20, 20, 20 )
+    authors.setWordWrap( True )
+    authors.setOpenExternalLinks( True )
+
+    tabwidget = QtGui.QTabWidget()
+    tabwidget.addTab( about, u"About" )
+    tabwidget.addTab( authors, u"Credits" )
+    s.layout().addWidget( tabwidget )
 
     button = QtGui.QPushButton( u"Ok" )
     s.layout().addWidget( button )

017b547b65ddaeaddf0a816ed921006784daf956
sundance <devnull@localhost>
2011-02-05 15:43:42 +0100

    Made mkresources script work more robustly on Win32 platforms.

--- resources/mkresources.py
+++ resources/mkresources.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import subprocess
 
 this_dir = os.path.dirname( __file__ )
 
@@ -9,6 +10,6 @@ PYRCC    = os.path.join( this_dir, "spyrcc.py" )
 RESOURCE = os.path.join( this_dir, "resources.qrc" )
 OUTPUT   = os.path.join( this_dir, "../resources.py" )
 
+cmdline = "%s -o %s %s" % ( PYRCC, OUTPUT, RESOURCE )
 
-
-os.execlp( PYRCC, PYRCC, "-o", OUTPUT, RESOURCE )
+subprocess.Popen( cmdline, shell=True )

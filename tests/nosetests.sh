eaa2eda15ddae0626d7b7bd43e20d1981d04bc75
Sundance <sundance@ierne.eu.org>
2020-05-04 16:22:39 +0100

    Remove nose-based test runner. nose is ancient and unmaintained.

--- tests/nosetests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-THIS_DIR=$(dirname $0)
-ARGS=$@
-if [[ -z $ARGS ]] ; then
-    ARGS="$THIS_DIR/../src $THIS_DIR/"
-fi
-
-nosetests --with-doctest --doctest-extension=.rst --doctest-options=+NORMALIZE_WHITESPACE,+ELLIPSIS,+DONT_ACCEPT_TRUE_FOR_1,+IGNORE_EXCEPTION_DETAIL,+REPORT_NDIFF,+REPORT_ONLY_FIRST_FAILURE -v $ARGS

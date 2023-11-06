ff6769a2ef95071a1485215bacc56a0c7bd496ac
P. Varet <p.varet@gmail.com>
2023-11-06 14:09:09 +0000

    Added object count stats to GC stats.

--- spyrit/gc_stats.py
+++ spyrit/gc_stats.py
@@ -39,12 +39,17 @@ class GCStats(QObject):
         periodicity: How frequently, in seconds, to compute and log the stats.
     """
 
+    _TOP_N = 10
+
     _dump_timer: QTimer
+    _obj_counts: dict[str, int]
 
     def __init__(
         self, parent: QObject, periodicity: int = constants.GC_STATS_DUMP_PERIOD
     ) -> None:
         super().__init__(parent)
+
+        self._obj_counts = {}
 
         self._dump_timer = QTimer(self)
         self._dump_timer.setInterval(periodicity * 1000)
@@ -55,16 +60,24 @@ class GCStats(QObject):
     def logGCStats(self) -> None:
         """
         Computes memory stats and logs them to the debug loglevel.
+
+        Also computes the increase in non-builtin objects between invocations,
+        and logs the top to the debug loglevel.
         """
 
         def _reducer(
-            current_tally: tuple[int, int],
+            current_tally: tuple[int, int, dict[str, int]],
             obj: object,
-        ) -> tuple[int, int]:
-            count, mem = current_tally
-            return (count + 1, mem + sys.getsizeof(obj))
+        ) -> tuple[int, int, dict[str, int]]:
+            count, mem, per_obj_count = current_tally
+            obj_name = type(obj).__module__ + "." + type(obj).__qualname__
+            per_obj_count.setdefault(obj_name, 0)
+            per_obj_count[obj_name] += 1
+            return (count + 1, mem + sys.getsizeof(obj), per_obj_count)
 
-        count, mem = reduce(_reducer, gc.get_objects(), (0, 0))
+        count, mem, per_obj_count = reduce(
+            _reducer, gc.get_objects(), (0, 0, {})
+        )
 
         count_string = f"{count:,}"
         mem_string = f"{mem:,}"
@@ -74,3 +87,26 @@ class GCStats(QObject):
             count_string,
             mem_string,
         )
+
+        _builtins_name = type(True).__module__
+
+        if self._obj_counts:
+            obj_count_diff: dict[str, int] = {}
+
+            for obj, count in per_obj_count.items():
+                obj_count_diff[obj] = count - self._obj_counts.get(obj, 0)
+
+            diff_counts = sorted(
+                [
+                    (count, obj)
+                    for obj, count in obj_count_diff.items()
+                    if obj.split(".")[0] != _builtins_name and count > 0
+                ]
+            )
+
+            if diff_counts:
+                logging.debug("Object count growth:")
+                for count, obj in diff_counts[: -(self._TOP_N + 1) : -1]:
+                    logging.debug(" - %s: +%s", obj, count)
+
+        self._obj_counts = per_obj_count

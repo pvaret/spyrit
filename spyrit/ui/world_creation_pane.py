a1f1b706f7b7f8c96d905dfe1950c48464cbd89f
P. Varet <p.varet@gmail.com>
2024-07-22 19:17:43 +0100

    Refactored world creation pane validation logic.

--- spyrit/ui/world_creation_pane.py
+++ spyrit/ui/world_creation_pane.py
@@ -18,13 +18,52 @@ Implements a UI to set up a new world.
 
 from typing import Any
 
-from PySide6.QtCore import Signal, Slot
+from PySide6.QtCore import QObject, Signal, Slot
 from PySide6.QtWidgets import QPushButton
 
 from spyrit.session.instance import SessionInstance
 from spyrit.settings.spyrit_settings import SpyritSettings
 from spyrit.ui.base_dialog_pane import BaseDialogPane
 from spyrit.ui.settings.server_settings_ui import ServerSettingsUI
+
+
+class SettingsValidator(QObject):
+    """
+    Monitors a settings object for changes and emits a signal when the settings
+    object's contents can be used to start a connection.
+
+    Args:
+        parent: Used for lifetime management.
+
+        settings: The settings object to monitor for changes and check for
+            validity.
+    """
+
+    # This signal is sent when the validity of the settings object's contents
+    # changes one way or another.
+
+    validityChanged: Signal = Signal(bool)
+
+    _valid: bool = False
+    _settings: SpyritSettings
+
+    def __init__(self, parent: QObject, settings: SpyritSettings) -> None:
+        super().__init__(parent)
+
+        self._settings = settings
+        settings.onUpdateCall(self.checkSettings)
+        self.checkSettings()
+
+    def checkSettings(self, _: Any = None) -> None:
+        if (valid := self.valid()) != self._valid:
+            self._valid = valid
+            self.validityChanged.emit(valid)
+
+    def valid(self) -> bool:
+        return (
+            self._settings.net.port.isSet()
+            and self._settings.net.server.isSet()
+        )
 
 
 class WorldCreationPane(BaseDialogPane):
@@ -45,6 +84,7 @@ class WorldCreationPane(BaseDialogPane):
     _world_settings: SpyritSettings
     _instance: SessionInstance
     _connect_button: QPushButton
+    _validator: SettingsValidator
 
     def __init__(
         self,
@@ -63,49 +103,23 @@ class WorldCreationPane(BaseDialogPane):
 
         self.setWidget(ServerSettingsUI(self._world_settings))
 
-        self._world_settings.onUpdateCall(self._maybeEnableConnectButton)
-        self._maybeEnableConnectButton()
+        ok_button.setEnabled(False)
+
+        self._validator = SettingsValidator(self, self._world_settings)
+        self._validator.validityChanged.connect(ok_button.setEnabled)
 
         self.okClicked.connect(self._openWorld)
 
     @Slot()
     def _openWorld(self) -> None:
         """
-        Creates a game pane for the newly configured world, and switches to it.
+        Signals that the settings in the pane should be used to open a game
+        world.
         """
 
-        if not self._areSettingsValid():
-            return
-
-        self._world_settings.setParent(self._root_settings)
-
-        self.openWorldRequested.emit(self._world_settings)
-
-    @Slot()
-    def _maybeEnableConnectButton(self, entity: Any = None) -> None:
-        """
-        Enables the connect button if the current world settings are valid.
-
-        Args:
-            entity: Which settings entity was updated, that triggered the call
-                to this function. Unused.
-        """
-
-        self._connect_button.setEnabled(self._areSettingsValid())
-
-    def _areSettingsValid(self) -> bool:
-        """
-        Validates the settings currently entered in the form.
-
-        Returns:
-            Whether the current world settings can be used to connect to a
-            world.
-        """
-
-        return (
-            self._world_settings.net.port.isSet()
-            and self._world_settings.net.server.isSet()
-        )
+        if self._validator.valid():
+            self._world_settings.setParent(self._root_settings)
+            self.openWorldRequested.emit(self._world_settings)
 
     def onActive(self) -> None:
         """
